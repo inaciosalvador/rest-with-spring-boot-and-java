cf9da53944a49260a0c7a7ab1149eaf94049368e
Inacio Salvador <inaciosalvador03@gmail.com>
2025-03-06 00:12:26 -0300

    Primeiro endpoint criado

---
+++ src/main/java/br/com/inacio/Sturtup.java
@@ -0,0 +1,13 @@
+package br.com.inacio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sturtup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sturtup.class, args);
+	}
+
+}
